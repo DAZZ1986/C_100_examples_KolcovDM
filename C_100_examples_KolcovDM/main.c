ee82f147904d24f11f146b0dd9e61d087bdb2e33
DAZZ1986 new git <dvdzebra@yandex.ru>
2022-02-24 14:39:32 +0300

    f c

---
+++ C_100_examples_KolcovDM/main.c
@@ -0,0 +1,52 @@
+#include <locale.h>
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+    char* locale = setlocale(LC_ALL, "");
+
+    printf("/////////// 100 примеров на Си Кольцов Д.М.\n");
+
+    //Сумма двух чисел (с.19)   
+    /*
+    printf("//////Введите два целых числа.\n");
+    int first, second, sum;
+    scanf_s("%d %d", &first, &second);
+
+    sum = first + second;
+    printf("%d + %d = %d \n", first, second, sum);
+    */
+
+
+    //Умножение двух вещественных чисел (с.20)
+    /*
+    double a, b, c;
+    scanf_s("%lf %lf", &a, &b);
+    c = a * b;
+    printf("%lf ", c);
+    */
+
+
+    //Определение ASCII значения символа (символ в dec) (с.21)
+    /*
+    char c;
+    printf("Введите символ:");
+
+    scanf_s("%c", &c);
+    printf("ASCII-код %c = %d \n", c, c);
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
